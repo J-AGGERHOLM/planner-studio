08ce773c065f236c143655a7d3845437ecc5e459
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-08-11 10:51:21 +0200

    Editor page, route and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\EditorController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
+
+Route::get('/editor', [EditorController::class, 'index']);
+
