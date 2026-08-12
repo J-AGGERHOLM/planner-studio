251bb6c20cf982d19638cf9ed480b4d8f54c8e22
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-08-12 12:20:27 +0200

    _refactor_readied layout for 3d view background

--- resources/js/pages/shared/Layout.vue
+++ resources/js/pages/shared/Layout.vue
@@ -1,15 +1,22 @@
 <template>
-    <section class="bg-gray-200">
-        <header class="flex items-center justify-between">
-            <h2 class="ml-6 flex pt-3 pb-3 text-xl font-light">
-                Planner - Studio
-            </h2>
-            <Nav></Nav>
-        </header>
-    </section>
-    <section class="m-2 flex gap-6">
-        <slot></slot>
-    </section>
+    <div class="absolute h-full min-w-screen">
+        <section class="bg-gray-200">
+            <header class="flex items-center justify-between">
+                <h2 class="ml-6 flex pt-3 pb-3 text-xl font-light">
+                    Planner - Studio
+                </h2>
+                <Nav></Nav>
+            </header>
+        </section>
+
+        <section class="flex justify-end">
+            <slot name="ui"></slot>
+        </section>
+    </div>
+
+    <main>
+        <slot> </slot>
+    </main>
 </template>
 
 <script>
