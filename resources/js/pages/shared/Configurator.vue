ab8aa11fb34be0dedb88910af110c9c54a25b4c0
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-08-12 15:08:54 +0200

    _feat_light set up

--- resources/js/pages/shared/Configurator.vue
+++ resources/js/pages/shared/Configurator.vue
@@ -4,8 +4,8 @@
 import * as THREE from 'three';
 /* import { OrbitControls } from 'jsm/controls/OrbitControls.js';
  */ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-/* import { UltraHDRLoader } from 'jsm/loaders/UltraHDRLoader.js';
- */
+import { UltraHDRLoader } from 'three/addons/loaders/UltraHDRLoader.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -14,31 +14,64 @@ const camera = new THREE.PerspectiveCamera(
     1000,
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 document.body.appendChild(renderer.domElement);
 
 /* Light Settings */
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 2);
-scene.add(hemiLight);
+const hdrLoader = new UltraHDRLoader();
+const texture = await hdrLoader.loadAsync('textures/empty_play_room_1k.jpg');
+texture.mapping = THREE.EquirectangularReflectionMapping;
+scene.environment = texture;
 
-const sunlight = new THREE.DirectionalLight(0xffffff, 1);
-sunlight.position.set(0, 0, 1);
-scene.add(sunlight);
+const spotLight = new THREE.SpotLight();
+
+spotLight.color.set(0xffffff);
+spotLight.intensity = 20;
+spotLight.distance = 10;
+spotLight.penumbra = 1;
+
+spotLight.position.set(0, 5, 2);
+spotLight.castShadow = true;
+
+spotLight.shadow.mapSize.set(1024, 1024);
+spotLight.shadow.radius = 5;
+
+scene.add(spotLight);
 
 /* const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping.true; */
 
+/* floor plane: */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(30, 20),
+    new THREE.ShadowMaterial({
+        color: 0x000000,
+        opacity: 0.25,
+    }),
+);
+
+floor.rotation.x = -Math.PI / 2;
+floor.position.y = -0;
+floor.receiveShadow = true;
+
+scene.add(floor);
+
 const loader = new GLTFLoader();
 const modelGlb = await loader.loadAsync('/models/hallingdal-547.glb');
 const model = modelGlb.scene;
-
 model.traverse((child) => {
-    if (child.isMesh) {
-        child.geometry.center();
+    if (child instanceof THREE.Mesh) {
+        child.castShadow = true;
     }
 });
+
 scene.add(model);
 
 camera.position.z = 1.5;
