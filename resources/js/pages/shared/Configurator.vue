20573caa62f24077d0de48454690e60d15da560d
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-08-12 13:16:57 +0200

    _feat_ loaded sofa  model into scene

--- resources/js/pages/shared/Configurator.vue
+++ resources/js/pages/shared/Configurator.vue
@@ -2,7 +2,10 @@
 
 <script>
 import * as THREE from 'three';
-
+/* import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+ */ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+/* import { UltraHDRLoader } from 'jsm/loaders/UltraHDRLoader.js';
+ */
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -15,16 +18,35 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+/* Light Settings */
 
-camera.position.z = 5;
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 2);
+scene.add(hemiLight);
+
+const sunlight = new THREE.DirectionalLight(0xffffff, 1);
+sunlight.position.set(0, 0, 1);
+scene.add(sunlight);
+
+/* const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping.true; */
+
+const loader = new GLTFLoader();
+const modelGlb = await loader.loadAsync('/models/hallingdal-547.glb');
+const model = modelGlb.scene;
+
+model.traverse((child) => {
+    if (child.isMesh) {
+        child.geometry.center();
+    }
+});
+scene.add(model);
+
+camera.position.z = 1.5;
+camera.position.y = 0.75;
+camera.rotation.x = -0.5;
 
 function animate(time) {
-    cube.rotation.x = time / 2000;
-    cube.rotation.y = time / 1000;
+    model.rotation.y = time / 5000;
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
