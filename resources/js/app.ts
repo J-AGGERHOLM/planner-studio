95d3a32acc777676457a83cb17b893ba7844deb8
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-08-13 13:34:13 +0200

    _WIP__ button component

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,18 @@
 import { createInertiaApp } from '@inertiajs/vue3';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBox, faGear, faPlus, faSliders } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+library.add(faBox, faGear, faPlus, faSliders);
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     progress: {
         color: '#4B5563',
+    },
+    withApp(app) {
+        app.component('FontAwesomeIcon', FontAwesomeIcon);
     },
 });
