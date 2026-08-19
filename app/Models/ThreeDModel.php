b0cfce2cf7540e1ff3bd1d00cf6d87a5e0dffe5d
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-08-19 12:44:38 +0200

    _fix_ fixed naming of model, migrated database to fix

--- app/Models/ThreeDModel.php
+++ app/Models/ThreeDModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class 3DModel extends Model
+class ThreeDModel extends Model
 {
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\EditorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EditorController::class, 'index'])->name('home');
+
