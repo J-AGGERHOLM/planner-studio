7e393d0fd0e5b3445b374fc80251c58a98b3f8d5
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-08-21 18:47:28 +0200

    _feat_ unique entries into modelSession, by replacing old entries matching by uuid

--- resources/js/util/modelManager.js
+++ resources/js/util/modelManager.js
@@ -1,6 +1,6 @@
 import { generateUUID } from 'three/src/math/MathUtils.js';
 
 export function generateEntryUUID() {
-    const entryId = generateUUID();
+    const entryId = generateUUID().toString();
     return entryId;
 }
